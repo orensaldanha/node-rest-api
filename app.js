7c79fcde2085d84e73b06404a8e056087977733d
Oren Saldanha <orensaldanha@gmail.com>
2022-09-11 12:09:18 +0530

    return json

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/json', (req, res) => {
+  res.json({data: "Hello World"});
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
